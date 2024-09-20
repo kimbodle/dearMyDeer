86c724c054d7d3785aa1e413d93cb0c8e637de33
kimbodle <103980156+kimbodle@users.noreply.github.com>
2024-09-20 17:07:56 +0900

    Environment, Design done

---
+++ Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    [SerializeField] float lightDecay = 0.1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minimunAngle = 40f;
+
+    Light myLight;
+
+    private void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+    private void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+    //πË≈Õ∏ÆΩ¿µÊ
+    public void RestoreLightAngle(float intensityAmount)
+    {
+        myLight.intensity += intensityAmount;
+    }
+    private void DecreaseLightIntensity()
+    {
+        myLight.intensity -=lightDecay * Time.deltaTime;
+    }
+
+    private void DecreaseLightAngle()
+    {
+        if(myLight.spotAngle <= minimunAngle)
+        {
+            return;
+        }
+        else
+        {
+            myLight.spotAngle -= angleDecay * Time.deltaTime;
+        }
+    }
+}
